1154da6fc35ae8f7bc1496e8c25f17b3428f3d09
Nithish P <nithish162965@gmail.com>
2024-03-24 21:45:25 +0530

    project 24 mar updated

--- app/src/main/java/com/example/horizon/FragmentHome.kt
+++ app/src/main/java/com/example/horizon/FragmentHome.kt
@@ -1,12 +1,11 @@
 package com.example.horizon
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 
 
 class FragmentHome : Fragment() {
